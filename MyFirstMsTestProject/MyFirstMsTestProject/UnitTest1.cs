d0824ec45754ed1d237fb70ec3655f362bd34cdf
angelov.st.anton <angelov.anton@yahoo.com>
2020-10-06 14:51:21 +0300

    refactor folder structure

---
+++ MyFirstMsTestProject/MyFirstMsTestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyFirstMsTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
