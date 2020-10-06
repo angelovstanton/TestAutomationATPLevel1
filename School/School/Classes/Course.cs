d85ab72ba1485453e0961e67b3ecdae629ddcbdb
Sheir Ali <sheir@magma.ca>
2020-10-06 10:00:34 -0400

    work in progress -- School exercise

---
+++ School/School/Classes/Course.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School.Classes
+{
+    public class Course
+    {
+        public Course(string name)
+        {
+            this.Name = name;
+            Students = new List<Student>();
+        }
+
+        private string _name = string.Empty;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty!");
+                }
+                _name = value;
+            }
+        }
+
+        private List<Student> Students { get; set; }
+
+        public void AddStudent(Student pupil)
+        {
+            if (pupil == null)
+            {
+                throw new ArgumentException("Student cannot be null!");
+            }
+            if (this.Students.Count > 30)
+            {
+                throw new ArgumentException("Course is full!");
+            }
+            this.Students.Add(pupil);
+        }
+    }
+}
---
+++ School/School/Classes/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School.Classes
+{
+    public class Student
+    {
+        private int _minId = 10000;
+        private int _maxId = 99999;
+
+        public Student(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        private string _name = string.Empty;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty!");
+                }                
+                _name = value;
+            }
+        }
+
+        int _studentId = 0;
+        public int Id
+        {
+            get 
+            { 
+                return _studentId; 
+            }
+            set
+            {
+                
+                if (value < _minId || value > _maxId)
+                {
+                    throw new ArgumentException($"Id must be between {_minId} and {_maxId}");
+                }
+                _studentId = value;
+            }
+        }
+    }
+}
